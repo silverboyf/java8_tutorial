b719af9ece2fc260e89ef4112e23cfefbdbdd4dc
silverboyf <silverboyf@10.12.57.212>
2015-02-13 15:02:47 +0900

    java8 튜토리얼 초기 구조 commit

---
+++ src/main/com/silverboyf/java8/lambda/LambdaSample.java
@@ -0,0 +1,10 @@
+package com.silverboyf.java8.lambda;
+
+public class LambdaSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/com/silverboyf/java8/stream/StreamApiSample.java
@@ -0,0 +1,10 @@
+package com.silverboyf.java8.stream;
+
+public class StreamApiSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
